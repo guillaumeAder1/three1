410bbf492dfe843d68702976f9dd9df4b64a7246
Guillaume Ader <guillaume@Guillaumes-MacBook-Pro-2.local>
2019-11-24 21:01:00 +0000

    new build and config

---
+++ precache-manifest.e61f136c48f9342d6e626b453d675843.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa12f885a1221304eea4",
+    "url": "/three1/css/app.554d40f2.css"
+  },
+  {
+    "revision": "5bc3a3b9f76ed4cd56b61ac00cad000e",
+    "url": "/three1/index.html"
+  },
+  {
+    "revision": "fa12f885a1221304eea4",
+    "url": "/three1/js/app.3970df0e.js"
+  },
+  {
+    "revision": "e6702c44d95f4c3aab72",
+    "url": "/three1/js/chunk-vendors.e9a84b50.js"
+  },
+  {
+    "revision": "cc164aa18b1aede24032841db2e11df4",
+    "url": "/three1/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/three1/robots.txt"
+  }
+]);
