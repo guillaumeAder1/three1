392b9254cf105a0137746ff2a40b697e34fe59b2
System Administrator <root@Guillaumes-MacBook-Pro-2.local>
2019-11-24 21:14:48 +0000

    new build and deploy

--- dist/precache-manifest.86d418ea534d3de25bec717cb8d3d53b.js
+++ dist/precache-manifest.86d418ea534d3de25bec717cb8d3d53b.js
@@ -1,15 +1,15 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "5f0a380c0470d28d28d5",
-    "url": "/three1/css/app.57f6e911.css"
+    "revision": "7c4541c749dacc766eee",
+    "url": "/three1/css/app.00487e76.css"
   },
   {
-    "revision": "c1c55602a26846aa8b8805157bf5948c",
+    "revision": "8a8a6879b32c83cfefe77258659c0acb",
     "url": "/three1/index.html"
   },
   {
-    "revision": "5f0a380c0470d28d28d5",
-    "url": "/three1/js/app.540f5694.js"
+    "revision": "7c4541c749dacc766eee",
+    "url": "/three1/js/app.7408654a.js"
   },
   {
     "revision": "e6702c44d95f4c3aab72",
